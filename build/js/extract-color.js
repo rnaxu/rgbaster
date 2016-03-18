396300288d60b4ea8f02a55b7ae88b02246860df
rnaxu <luv.s2.rnaxu.gmail.com>
2016-03-18 17:35:30 +0900

    first commit

---
+++ build/js/extract-color.js
@@ -0,0 +1,12 @@
+var sampleImage = document.getElementById('sample');
+
+RGBaster.colors(sampleImage, {
+  paletteSize: 30,　
+  exclude: [ 'rgb(0,0,0)' ],
+  success: function(payload) {
+    console.log(payload.dominant);
+    console.log(payload.secondary);
+    console.log(payload.palette);
+    $('.js-color').css({backgroundColor:payload.dominant});
+  }
+});
